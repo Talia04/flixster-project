232d8ad32b859e0c4ae133824f1cec507697fa86
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-16 21:33:18 -0700

    Added movie description pop-up on hover

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 const apiKey = 'c666be630baaddcdc7f5bb1eac153e04';
 let moviePage = 1;
-let movieData = {};
+let movieData = {}; // Dictionary to store movie data
 
 const moviesContainer = document.getElementById('movies-grid');
 const closeSearchButton = document.getElementById('search-icon');
 
+// Fetch data from the API using async/await
 const fetchData = async () => {
   const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${moviePage}`;
-  
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
+    // Handle the retrieved data here
+    console.log(data);
 
     if (moviePage === 1) {
       movieData = data;
@@ -20,66 +22,84 @@ const fetchData = async () => {
 
     displayMovies(data);
   } catch (error) {
+    // Handle any errors that occur during the request
     console.log('Error:', error);
   }
 };
 
+// Function to process and display the movie information
 function displayMovies(data) {
+  // Access the movie details from the API response
   const movies = data.results;
 
   movies.forEach(movie => {
-    const { title, poster_path: poster, vote_average: votes, overview: description } = movie;
+    // Extract relevant movie information (title, poster, votes)
+    const title = movie.title;
     const baseUrl = 'http://image.tmdb.org/t/p/';
+    const poster = movie.poster_path;
+    const votes = movie.vote_average;
+    const description = movie.overview;
 
+
+    // Dynamically Create HTML elements to display movie details
     const movieInfo = document.createElement('div');
     movieInfo.className = 'movie-card';
-
     const movieTitle = document.createElement('h2');
     movieTitle.className = 'movie-title';
-    movieTitle.textContent = title;
-
     const moviePoster = document.createElement('img');
     moviePoster.className = 'movie-poster';
-    moviePoster.alt = 'Movie Poster';
-    moviePoster.src = `${baseUrl}w300${poster}`;
-
+    moviePoster.alt = "Movie Poster";
     const movieVotes = document.createElement('h3');
     movieVotes.className = 'movie-votes';
-    movieVotes.textContent = `⭐️ ${votes}`;
-
     const movieDescription = document.createElement('div');
     movieDescription.className = 'movie-description';
+
+    // Set the movie details to the created elements
+    movieTitle.textContent = title;
+    moviePoster.src = baseUrl + 'w300' + poster;
+    movieVotes.textContent = '⭐️       ' + votes;
     movieDescription.textContent = description;
 
-    const closeButton = document.createElement('span');
-    closeButton.className = 'close-button';
-    closeButton.innerHTML = '&times;';
 
-    closeButton.addEventListener('click', () => {
-      movieDescription.style.display = 'none';
-    });
-
-    movieDescription.appendChild(closeButton);
-
+    // Append the movie info to the movie card
     movieInfo.appendChild(moviePoster);
     movieInfo.appendChild(movieTitle);
     movieInfo.appendChild(movieVotes);
     movieInfo.appendChild(movieDescription);
 
+    // Event listeners for hover and timeout
     movieInfo.addEventListener('mouseenter', () => {
-      movieInfo.timerId = setTimeout(() => {
-        movieDescription.style.display = 'block';
-      }, 1000);
+        // Show the movie description after 1 second
+        movieInfo.timerId = setTimeout(() => {
+          movieDescription.style.display = 'block';
+        }, 1000);
+      });
+  
+      movieInfo.addEventListener('mouseleave', () => {
+        // Clear the timer and hide the movie description
+        clearTimeout(movieInfo.timerId);
+        movieDescription.style.display = 'none';
+      });
+
+    const closeButton = document.createElement('span');
+    closeButton.className = 'close-button';
+    closeButton.innerHTML = '&times;';
+
+    // Add event listener to close the pop-up
+    closeButton.addEventListener('click', () => {
+    movieDescription.style.display = 'none';
     });
 
-    movieInfo.addEventListener('mouseleave', () => {
-      clearTimeout(movieInfo.timerId);
-      movieDescription.style.display = 'none';
-    });
+    movieDescription.appendChild(closeButton);
 
+
+    // Append the movie card to the movie container
     moviesContainer.appendChild(movieInfo);
   });
 }
+
+// Call the fetchData function to fetch and display the movie information
+fetchData();
 
 const loadMoreButton = document.getElementById('load-more-movies-btn');
 
@@ -111,8 +131,10 @@ loadMoreButton.addEventListener('click', loadMoreMovies);
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
 
+// Event listener for the search input
 searchInput.addEventListener('keydown', event => {
   if (event.key === 'Enter') {
+    console.log(searchInput.value);
     performMovieSearch(searchInput.value);
   }
 });
@@ -121,24 +143,27 @@ searchBtn.addEventListener('click', event => {
   performMovieSearch(searchInput.value);
 });
 
-const performMovieSearch = async query => {
+async function performMovieSearch(query) {
+  // Clear existing movies
   clearMovies();
-  moviePage = 1;
-  movieData = {};
+  moviePage = 1; // Reset movie page
+  movieData = {}; // Reset movie data dictionary
 
   const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
   try {
+    console.log('Hello');
     const response = await fetch(searchUrl);
     const data = await response.json();
-
+    console.log(data);
     movieData = data;
     displayMovies(movieData);
   } catch (error) {
     console.log('Error:', error);
   }
-};
+}
 
+// Event listener for the close search button
 closeSearchButton.addEventListener('click', event => {
   clearMovies();
   closeSearch();
@@ -149,8 +174,9 @@ function clearMovies() {
 }
 
 function closeSearch() {
+  // Clear search input
   searchInput.value = '';
+
+  // Reload current movies
   fetchData();
 }
-
-fetchData();
