c2e9013a2be88f274d2cd7419fd9037724501328
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-16 21:29:29 -0700

    Added pop-up description for movie cards on hover

--- README.md
+++ README.md
@@ -41,29 +41,33 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 ### Walkthrough Video
 
-`TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
+<img src="https://github.com/Talia04/flixster-project/raw/main/FlixsterWalkthrough.gif" alt="Flixster Walkthrough" width="100%">
 
-`ADD_EMBEDDED_CODE_HERE`
-![Flixster Walkthrough](https://github.com/Talia04/flixster-project/raw/main/FlixsterWalkthrough.gif)
 
 ### Reflection
 
 - Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
 
-Add your response here
+I found the giphy lab to be very helpful since I used what I learned about APIs to help me complete my project. I had to build on the little I learnt on that lab which esseantially got me started on the FLixster project. Also, the mini demo on git really helped a lot as well, because I was able to do all my commits and code pushes based off of the quick session on using git on the terminal
 
 - If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
   
-Add your response here
+-I would have gone on to add functionality that would allow the user to click on a movie card to view additional info about a movie while clearing the display of other movies. The user would also be able to exit out of the selected movie and go back to the rest of the movies, essentially picking up where they left off
+-I would also go on to allow user to pick movie categories
+Another, possibly more complicated functionality would be to allow filtered against different criteria such as votes, release date etc
 
 - Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
 
 Add your response here
+-I think my implementation of the core features went well. I managed to display a list of movies,load more movies, search movies using a keyword, exit out of the search and return to original movie display. I was also able to add a display pop-up for the movie description that appears when a user hovers over a movie cars for about 3 seconds. I think there is still lots of room for improvement on that feature, concerning the way it appears in the UI and its positioning. Overally, the UI looks ok, but some elements may need further polishing, such as the Search button and the search input box. 
+
 
 ### Open-source libraries used
 
-- Add any links to open-source libraries used in your project.
+- ChatGPT for most questions
+- W3Schools website
 
 ### Shout out
 
-Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+I distinctly remember Diogo helped me twice to fix bugs in my code when it was not working as intended
+I got help from several of my peers but it is hard to remember who helped with what exactly. Most of us were working collaboratively. I vaguely remember Daniel helping me out with the API key for the movies, Emmanuella helped me with hosting my page using github pages, and for the most part, some of my peers helped me answer some quick questions that helped me complete this project
--- test.js
+++ test.js
@@ -1,18 +1,16 @@
 const apiKey = 'c666be630baaddcdc7f5bb1eac153e04';
 let moviePage = 1;
-let movieData = {}; // Dictionary to store movie data
+let movieData = {};
 
 const moviesContainer = document.getElementById('movies-grid');
 const closeSearchButton = document.getElementById('search-icon');
 
-// Fetch data from the API using async/await
 const fetchData = async () => {
-  const apiUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&page=${moviePage}`;
+  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${moviePage}`;
+  
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    // Handle the retrieved data here
-    console.log(data);
 
     if (moviePage === 1) {
       movieData = data;
@@ -20,52 +18,68 @@ const fetchData = async () => {
       movieData.results.push(...data.results);
     }
 
-    displayMovies(movieData);
+    displayMovies(data);
   } catch (error) {
-    // Handle any errors that occur during the request
     console.log('Error:', error);
   }
 };
 
-// Function to process and display the movie information
 function displayMovies(data) {
-  // Access the movie details from the API response
   const movies = data.results;
 
   movies.forEach(movie => {
-    // Extract relevant movie information (title, poster, votes)
-    const title = movie.title;
+    const { title, poster_path: poster, vote_average: votes, overview: description } = movie;
     const baseUrl = 'http://image.tmdb.org/t/p/';
-    const poster = movie.poster_path;
-    const votes = movie.vote_average;
 
-    // Dynamically Create HTML elements to display movie details
     const movieInfo = document.createElement('div');
     movieInfo.className = 'movie-card';
+
     const movieTitle = document.createElement('h2');
     movieTitle.className = 'movie-title';
+    movieTitle.textContent = title;
+
     const moviePoster = document.createElement('img');
     moviePoster.className = 'movie-poster';
+    moviePoster.alt = 'Movie Poster';
+    moviePoster.src = `${baseUrl}w300${poster}`;
+
     const movieVotes = document.createElement('h3');
     movieVotes.className = 'movie-votes';
+    movieVotes.textContent = `⭐️ ${votes}`;
 
-    // Set the movie details to the created elements
-    movieTitle.textContent = title;
-    moviePoster.src = baseUrl + 'w300' + poster;
-    movieVotes.textContent = '⭐️       ' + votes;
+    const movieDescription = document.createElement('div');
+    movieDescription.className = 'movie-description';
+    movieDescription.textContent = description;
 
-    // Append the movie info to the movie card
+    const closeButton = document.createElement('span');
+    closeButton.className = 'close-button';
+    closeButton.innerHTML = '&times;';
+
+    closeButton.addEventListener('click', () => {
+      movieDescription.style.display = 'none';
+    });
+
+    movieDescription.appendChild(closeButton);
+
     movieInfo.appendChild(moviePoster);
     movieInfo.appendChild(movieTitle);
     movieInfo.appendChild(movieVotes);
+    movieInfo.appendChild(movieDescription);
 
-    // Append the movie card to the movie container
+    movieInfo.addEventListener('mouseenter', () => {
+      movieInfo.timerId = setTimeout(() => {
+        movieDescription.style.display = 'block';
+      }, 1000);
+    });
+
+    movieInfo.addEventListener('mouseleave', () => {
+      clearTimeout(movieInfo.timerId);
+      movieDescription.style.display = 'none';
+    });
+
     moviesContainer.appendChild(movieInfo);
   });
 }
-
-// Call the fetchData function to fetch and display the movie information
-fetchData();
 
 const loadMoreButton = document.getElementById('load-more-movies-btn');
 
@@ -86,7 +100,7 @@ const loadMoreMovies = async () => {
       movieData.results.push(...data.results);
     }
 
-    displayMovies(movieData);
+    displayMovies(data);
   } catch (error) {
     console.log('Error:', error);
   }
@@ -97,10 +111,8 @@ loadMoreButton.addEventListener('click', loadMoreMovies);
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
 
-// Event listener for the search input
 searchInput.addEventListener('keydown', event => {
   if (event.key === 'Enter') {
-    console.log(searchInput.value);
     performMovieSearch(searchInput.value);
   }
 });
@@ -109,27 +121,24 @@ searchBtn.addEventListener('click', event => {
   performMovieSearch(searchInput.value);
 });
 
-async function performMovieSearch(query) {
-  // Clear existing movies
+const performMovieSearch = async query => {
   clearMovies();
-  moviePage = 1; // Reset movie page
-  movieData = {}; // Reset movie data dictionary
+  moviePage = 1;
+  movieData = {};
 
   const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
   try {
-    console.log('Hello');
     const response = await fetch(searchUrl);
     const data = await response.json();
-    console.log(data);
+
     movieData = data;
     displayMovies(movieData);
   } catch (error) {
     console.log('Error:', error);
   }
-}
+};
 
-// Event listener for the close search button
 closeSearchButton.addEventListener('click', event => {
   clearMovies();
   closeSearch();
@@ -140,9 +149,8 @@ function clearMovies() {
 }
 
 function closeSearch() {
-  // Clear search input
   searchInput.value = '';
-
-  // Reload current movies
   fetchData();
 }
+
+fetchData();
